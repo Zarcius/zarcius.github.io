be94242eb6c229cea377459f075bcda555c58e4a
bfeldha <44585347+bfeldha@users.noreply.github.com>
2020-01-08 20:33:13 -0600

    i made spinny buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('rotation', [
+      state('default', style({ transform: 'rotate(0)' })),
+      state('rotated', style({ transform: 'rotate(360deg)'})),
+      transition('rotated -> default', animate('2000ms ease-out')),
+      transition('default -> rotated', animate('2000ms ease-in'))
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'spinnyButtons';
+  state = 'default';
+
+  spinTheButton() {
+    this.state = (this.state === 'default' ? 'rotated' : 'default');
+  }
 }
